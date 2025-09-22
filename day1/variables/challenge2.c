452312c59a941ca8414327aa3787a3405001217b
khaoula khaldi <khaoula2417@gmail.com>
2025-09-22 16:07:07 +0100

    realisation de challenge 2

---
+++ day1/variables/challenge2.c
@@ -0,0 +1,17 @@
+
+
+//challenge 2
+#include <stdio.h>
+
+int main(){
+    float c,k;
+    float temperateur;
+    printf ("veullier entre la temperateur en celusious: ");
+    scanf("%f",&c);
+    k=273.15;
+    temperateur=c+k;
+    printf("voila la temperateur en kelvin : %f",temperateur);
+
+
+
+    }
