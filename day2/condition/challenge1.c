c272db5649a8fd3fda9d067b7131f9ce631d259a
khaoula khaldi <khaoula2417@gmail.com>
2025-09-23 16:22:15 +0100

    realisatiom tab 1

--- day2/condition/challenge1.c
+++ day2/condition/challenge1.c
@@ -1,27 +1,17 @@
-//challeng 1
 
 
-// 
+// challenge 1
 #include <stdio.h>
 
 int main() {
-    float revenu;
-    int score_de_credit;
-    int duree_du_pret;
-    printf(" entrez le revenu annuel ");
-    scanf("%f",&revenu);
-    printf(" entrez le scorde de credit  ");
-    scanf("%d",&score_de_credit);
-    printf(" entrez la duree du pert  ");
-    scanf("%d",&duree_du_pret);
-    if (revenu >= 30000 && score_de_credit >= 700 && duree_du_pret <= 10){
-        printf ("vout etes eligible pour le pret \n");
-    }else if (revenu >= 30000 && score_de_credit >= 650 && duree_du_pret <= 15){
-        printf("vout etes eligible pour le pret avec des condittion \n");
-    }else{
-        printf("tu n est pas eligible");
+    int tab={};
+    int n,i;
+    printf("entres lindice de ce tab :");
+    scanf("%d",&n);
+    for (i=0;i<n;i++){
+        printf (" entres les elment de tab : ");
+        scanf("%d",&tab);
     }
-    
-
+    printf (" fin de programme");
     return 0;
 }
