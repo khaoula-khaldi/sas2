9ffc436717ba15b21d911ad7e650c9ddb07ce2e9
khaoula khaldi <khaoula2417@gmail.com>
2025-09-30 11:35:07 +0100

    trie dun tableaux

---
+++ sas/mini_projet/insertion.c/challengre2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int stok;
+    int j,i;
+    int tab[]={25,45,1,3,87,69};
+    for ( i=0;i<6;i++){
+        stok=tab[i];
+        j=i-1;
+        while(j>=0 && tab[j]>stok){
+            tab[j+1]=tab[j];
+            j--;
+        }
+        tab[j+1]=stok;
+    }
+    for(int i = 0 ; i<6 ; i++){
+        
+    printf(" %d",tab[i]);
+    } 
+
+    return 0;
+}
